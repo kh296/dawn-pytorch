0be603164fe26d2534113392a2e869c0a558a54b
kh296 <kh296@users.noreply.github.com>
2026-06-26 13:42:55 +0100

    Added device check following installation

--- scripts/pytorch_install.sh
+++ scripts/pytorch_install.sh
@@ -323,12 +323,45 @@ eval "${CMD}"
 
 T1=${SECONDS}
 
-# Check installation.
+# Check imports.
 echo ""
 echo "Performing initial imports:"
 CMD="python -c 'import torch; import torchaudio; import torchvision'"
 echo "${CMD}"
 eval "${CMD}"
+
+# Check devices.
+echo ""
+echo "Checking devices:"
+python - <<EOF
+"""
+Python code for checking devices seen by torch.
+"""
+import importlib
+import socket
+import torch
+
+print(f"\nDevices seen by torch on {socket.gethostname()}:")
+devices = ["xpu", "cuda", "mps", "cpu"]
+len_device_field = max(len(device) for device in devices)
+
+for device in devices:
+    try:
+        device_module = importlib.import_module(f"torch.{device}")
+    except ModuleNotFoundError:
+        device_module = None
+    if getattr(device_module, "is_available", lambda: False)():
+        n_device = device_module.device_count()
+    else:
+        n_device = 0
+
+    print(f"{device:<{len_device_field}} : {n_device}")
+    for idx in range(n_device):
+        try:
+            print(f"    {device_module.get_device_properties(idx).name}")
+        except AttributeError:
+            pass
+EOF
 T2=${SECONDS}
 
 echo ""
